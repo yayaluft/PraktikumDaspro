e87e613c3df8501e9bd23e6a204dd4368118d167
yayaluft <syanuha66@gmail.com>
2025-10-10 17:17:49 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS11.java
+++ jobsheet5/ifCetakKRS11.java
@@ -5,12 +5,7 @@ public class ifCetakKRS11{
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.println("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
+            System.out.println( uktLunas ? "Pembayaran UKT terverifikasi \nSilakan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
     }
 }
  
