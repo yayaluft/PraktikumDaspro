7515cd6ef8d89d973a5f336d79779a5be2f18463
yayaluft <syanuha66@gmail.com>
2025-10-10 12:04:43 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS11.java
+++ jobsheet5/ifCetakKRS11.java
@@ -8,7 +8,9 @@ public class ifCetakKRS11{
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-        } 
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+        }
     }
 }
  
